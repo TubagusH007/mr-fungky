6d0447a624f8e0fc1175d1c818b54fe9bdd2500f
unknown <tubagussains07@gmail.com>
2026-05-03 12:08:54 +0700

    Fix: update ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ly5jlr606vfxy926f=2b*&94=brxzrr+z#f)yxo5pq&o6wk)n1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
